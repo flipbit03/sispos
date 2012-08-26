2fab9c5992dd9f9cf83fdad504249ae547f1ab34
cadu.coelho <cadu.coelho@e19aeeef-e103-e014-d4d3-001e3d918057>
2012-08-26 16:55:34 +0000

    HTipo0G added, stable.

--- htipo0g.py
+++ htipo0g.py
@@ -37,6 +37,11 @@ background-color: red;
 {
 }
 
+.groupedhour
+{
+color: blue;
+}
+
 .hiliteativ
 {
     color: Red;
@@ -67,13 +72,13 @@ background-color: red;
     ('140000000','835520000','830140000')),
     
     ('ELETRO-PONTO1','ELETRONUCLEAR', 
-    ('350000000')),
+    ('350000000',)),
     
     ('ELETRO-PONTO2','ELETRONUCLEAR', 
-    ('360000000')),
+    ('360000000',)),
     
     ('ELETRO-PONTO3','ELETRONUCLEAR', 
-    ('260040000'))
+    ('260040000',))
     
     )
 
@@ -168,6 +173,7 @@ background-color: red;
         #sum tothora to the appropriate category inside its client    
         self.jdata[aliasedcliente][cargocat] += float(tothora)
         
+        # Round to 2 decimal places
         self.jdata[aliasedcliente][cargocat] = round(self.jdata[aliasedcliente][cargocat], 2)
         
     def process (self, f):
@@ -181,13 +187,116 @@ background-color: red;
 
         
         # Get output file for HTML
-        #o1 = self.getoutputfile(ext='html', append='%s-%s' % (f['#MES'], f['#ANO']))
+        o1 = self.getoutputfile(ext='html', append='%s-%s' % (f['#MES'], f['#ANO']))
         # Get output file for CSV
-        #o2 = self.getoutputfile(ext='csv', append='%s-%s-excel' % (f['#MES'], f['#ANO']))
+        o2 = self.getoutputfile(ext='csv', append='%s-%s-excel' % (f['#MES'], f['#ANO']))
+        
+        
+        # HTML, Start headers
+        o1.write('<!DOCTYPE html>')
+        o1.write('<html>')
+        o1.write('<head><title>%s - SISPOS</title><style>%s</style>' % (self.__class__.__name__.upper(), self.inlinecss))
+        o1.write('<body>')
+        o1.write('<h1>%s (%s/%s)</h1>' % (self.__class__.__name__.upper(),f['#MES'], f['#ANO']))
+        
+        # HTML, Open table
+        o1.write('<table border="0">')
+        o1.write('<tr>')
+        o1.write("""<th width="100">codped</th>
+                <th width="220">Cliente</th>
+                <th width="80">Cargo</th>
+                <th width="10">Htipo</th>
+                <th width="80">Horas</th>
+                <th width="250">Especialidade:</th>
+                <th width="250">Cliente considerado:</th>""")
+        o1.write('</tr>')
         
         for l in fs:
+            # split values       
+            codped, cliente, cargo, htipo, tothora, ign = l
+        
+            # Process line and build statistics.
             self.processjdata(l)
-            print l, self.judgecliente(l), self.judgecargo(l)
+            
+            # HTML
+            jcl = self.judgecliente(l)
+            jca = self.judgecargo(l)
+            
+            
+            # Alter formatting from outcome (cat/catmotiv/wts)
+            if jca == self.catUNK:
+                trclass = 'unknownhour'
+                trtitle = "verificar se faltam regras ou se regras com erro!"
+            else:
+                # If 'judgeclient' is different from cliente, then it's a grouping and should be highlighted
+                if jcl != cliente:
+                    trclass = "groupedhour"
+                else:
+                    trclass = "normalhour"
+                trtitle = ""
+                
+                # if the client is modified by renclientdata, explain and show what OS's are inside this group
+                renclientoslist = [x[2] for x in self.renclientdata if x[0] == jcl]
+                if renclientoslist:
+                    oslisttxt = ' '.join(renclientoslist[0])
+                    trtitle = "(Grupo %s) --> OS %s" % (jcl, oslisttxt)
+
+            # Write it out
+            o1.write('<tr class="%s" title="%s">' % (trclass, trtitle))
+            o1.write('<td>%s</td>' % (codped))
+            o1.write('<td>%s</td>' % (cliente))
+            o1.write('<td>%s</td>' % (cargo))
+            o1.write('<td align="center">%s</td>' % (htipo))
+            o1.write('<td align="left">%s</td>' % (tothora))
+            o1.write('<td align="left">%s</td>' % (jca,))
+            o1.write('<td align="left">%s</td>' % (jcl,))
+            o1.write('</tr>')
+            
+        # Close HTML Table
+        o1.write('</table>')
+        
+        o1.write('<hr />')
+        
+        # Print outcome tables in HTML and CSV
+        totclientes = self.jdata.keys()
+        totclientes.sort()
+        for clnome in totclientes:
+            clespecs = self.jdata[clnome].keys()
+            clespecs.sort()
+            totclhora = 0
+        
+            gclientoslist = ""
+            # if the client is modified by renclientdata, explain and show what OS's are inside this group
+            renclientoslist = [x[2] for x in self.renclientdata if x[0] == clnome]
+            if renclientoslist:
+                oslisttxt = ' '.join(renclientoslist[0])
+                gclientoslist = "[ %s ]" % (oslisttxt)
+
+            #CSV
+            o2.write('%s\t\t%s\r\n' % (clnome, gclientoslist))
+            
+            #HTML        
+            o1.write('<table border="1">')
+            o1.write('<tr><td colspan="2"><b>%s</b> <i>%s</i></td></tr>' % (clnome,gclientoslist))
+            o1.write('<tr><td>Especialidade: </td><td>Total de Horas: </td></tr>')
+            for espec in clespecs:
+                o1.write('<tr><td>%s</td><td>%s</td></tr>' % (espec, self.jdata[clnome][espec]))
+                totclhora += self.jdata[clnome][espec]
+                
+                #CSV
+                o2.write('%s\t%s\r\n' % (espec, str(self.jdata[clnome][espec]).replace('.',',')))
+                
+                
+            #CSV
+            o2.write('\r\n-\r\n-\r\n')
+                
+            #HTML    
+            o1.write('<tr><td colspan="2" align="center">TOTAL: %.2f</td></tr>' % (totclhora))
+            o1.write('</table>')
+            o1.write('<br />')
+        
+        # Close HTML document
+        o1.write('</body></html>')
 
         return self.jdata
 
